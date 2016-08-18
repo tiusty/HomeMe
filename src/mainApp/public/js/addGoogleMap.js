d59671faccea05c107d953df2d8ab9776244fecd
Cloud User <centos@ip-172-31-18-46.us-west-2.compute.internal>
2016-08-18 03:16:24 +0000

    The distanceMatrix is working with two hardcoded values. Next: validate address form in survey then have it load for the destination address of the distanceMatrix

--- src/mainApp/public/js/addGoogleMap.js
+++ src/mainApp/public/js/addGoogleMap.js
@@ -1,7 +1,8 @@
 function initMap() {
 	var mapDiv = document.getElementById('map');
-	var mpa = new google.maps.Map(mapDiv, {
+	var map = new google.maps.Map(mapDiv, {
 		center: {lat:42.3601, lng: -71.0589},
 		zoom: 8
 	});
+	distanceMatrix();
 }
