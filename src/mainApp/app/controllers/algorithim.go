cad52d63deffa32d94ba4286a9906c4a00dd516d
Cloud User <centos@ip-172-31-18-46.us-west-2.compute.internal>
2016-09-17 17:34:20 +0000

    Made some changes to the Algorithim file, going to start using scp from Idea IDE

--- src/mainApp/app/controllers/algorithim.go
+++ src/mainApp/app/controllers/algorithim.go
@@ -1,9 +1,13 @@
 package controllers
 
+import (
+	"github.com/revel/revel"
+)
+
 type Algorithim struct {
 	Application
 }
 
-func (c Algorithim) Index() {
+func (c Algorithim) Index() revel.Result {
 	return c.Render()
 }
